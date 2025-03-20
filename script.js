8fe2b8f5e5434585d5a891649b6e9bac569e212b
RoomofR <alawajiryan@gmail.com>
2025-03-20 11:25:12 +0200

    Added basic item search function

--- script.js
+++ script.js
@@ -4,9 +4,69 @@ console.log(`FFXIV Crafter for FFXIV Data version ${FFXIV_VERSION}`, "meow");
 //===========================================================================
 
 const ItemData = await getFFXIVItemData(FFXIV_VERSION);
+const Items_Keys = Object.keys(ItemData.items)
 
+//== View Variables =========================================================
+let searchString = "";
+let searchResults = [];
 
+//== View ===================================================================
+var root = document.getElementById("app");
+m.mount(root,{
+	view: ()=>{
+		return m("div", {id: "searchBox"},
+		[
+			m("label", {for: "search_text_box"},`Search (${searchResults.length})`),
+			m("input", {id: "search_text_box", type: "text", onkeyup: onSearchKey}),
+			m("table", searchResults.map(item =>
+				m("tr",[
+					m("td", item.id),
+					m("td", item.name),
+					m("td", {class: "addBtn", onclick: ()=>{addItem(item.id)}} ,"[+]")
+				])
+			)),
+		]);
+	}
+});
 
+//== Event Functions ========================================================
+
+let searchBarTimout = null;
+function onSearchKey(event){
+	searchString = event.target.value;
+	clearTimeout(searchBarTimout);
+	searchBarTimout = setTimeout(searchItems, 1000);
+}
+
+const MAX_SEARCH_RESULTS = 50;
+function searchItems(){
+	searchResults = [];
+
+	//Don't search if blank
+	if(searchString.trim() == "") return;
+
+	const itemNameRegex = new RegExp(searchString,"i");
+
+	let result_count = 0;
+	for(let item_id of Items_Keys){
+		if(result_count >= MAX_SEARCH_RESULTS) break;
+
+		let item = ItemData.items[item_id];
+		let item_name = item.name;
+
+		if(itemNameRegex.test(item_name)){
+			searchResults.push(item)
+			result_count++;
+		}
+	}
+	m.redraw();
+}
+
+function addItem(item){
+	console.log("adding",item)
+}
+
+//== Functions ==============================================================
 async function getFFXIVItemData(ffxivVersion){
 	return new Promise((resolve, reject) => {
 		protobuf.load("./data/data.proto", (err,root)=>{
